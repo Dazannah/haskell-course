3a5c74f95cbdb79a82edb47848d6adcc5244ba7f
Dazannah <davidfabian@freemail.hu>
2022-09-20 13:43:12 +0000

    Homework02 correction

--- Homework/Homework02/Homework02.hs
+++ Homework/Homework02/Homework02.hs
@@ -7,14 +7,11 @@ f1 x y z = x ** (y/z)
 f2 :: Float -> Float -> Float -> Float
 f2 x y z = sqrt (x/y - z)
 
-f3 :: Char -> [Char] -> [Char] -> [Char]
-f3 x y z = x:(show y ++ z)
+f3 :: Bool -> Bool -> [Bool]
+f3 x y = [x == True] ++ [y]
 
-f4 :: Bool -> Bool -> Bool -> [Bool]
-f4 x y z = [x > y] ++ [z]
-
-f5 :: [Float] -> [Float] -> [Float] -> Bool
-f5 x y z = x == (y ++ z)
+f4 :: String -> String -> String -> Bool
+f4 x y z = x == (y ++ z)
 
 -- Question 2
 -- Are really all variables in Haskell immutable? Try googling for the answer.
